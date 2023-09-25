efdddc2b0a0c62ca8cf973536e17cff6193b26cf
Jose Eduardo Perez <JosEd23>
2023-09-24 22:12:20 -0600

    Cambios en el backend

--- controllers/ClienteController.js
+++ controllers/ClienteController.js
@@ -101,7 +101,6 @@ const listar_clientes_filtro_admin = async function(req, res) {
         if (req.user.rol == 'ADMIN') {
             let filtro = req.params['filtro'];
 
-            console.log(filtro);
 
             if (filtro == null || filtro === 'null') {
                 let reg = await Cliente.find();
@@ -124,6 +123,24 @@ const listar_clientes_filtro_admin = async function(req, res) {
     }
 }
 
+const registro_cliente_admin = async function(req, res){
+    if(req.user){
+        if(req.user.rol == 'ADMIN'){
+            var data = req.body;
+
+            bcrypt.hash('123456789', null,null,async function(err,hash){
+                if(hash){
+                    data.password = hash;
+                    let reg = await Cliente.create(data);
+                    res.status(200).send({data:reg});
+                }else{
+                    res.status(200).send({message:'Error en el servidor', data: undefined});
+                }
+            })
+
+        }
+    }
+}
 
 const eliminar_cliente_dmin = async function(req,res){
     if(req.user){
@@ -144,5 +161,6 @@ const eliminar_cliente_dmin = async function(req,res){
 module.exports = {
     registro_cliete,
     login_cliente,
-    listar_clientes_filtro_admin
+    listar_clientes_filtro_admin,
+    registro_cliente_admin
 }
--- routes/cliente.js
+++ routes/cliente.js
@@ -10,6 +10,7 @@ var auth = require('../middlewares/authenticate');
 api.post('/registro_cliete',clienteController.registro_cliete);
 api.post('/login_cliente',clienteController.login_cliente);
 api.get('/listar_clientes_filtro_admin/:tipo/:filtro?',auth.auth,clienteController.listar_clientes_filtro_admin);
+api.post('/registro_cliente_admin',auth.auth,clienteController.registro_cliente_admin);
 
 
 module.exports = api;
