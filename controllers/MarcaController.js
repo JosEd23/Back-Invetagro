8f99d965138f0cdb0a1f0ecd607684c4890aa146
Jose Eduardo Perez <JosEd23>
2023-10-02 18:42:46 -0600

    Se anadio el  mantenimiento de categoria y marcas al modulo de compras

--- controllers/MarcaController.js
+++ controllers/MarcaController.js
@@ -17,7 +17,7 @@ const registro_marca_admin = async function (req, res){
     }
 
 }
-
+    
 //Listar marca
 const listar_marca_filtro_admin = async function(req, res) {
     if (req.user) {
@@ -76,7 +76,7 @@ const actualizar_marca_admin = async function(req, res){
             var data = req.body;
 
             var reg = await marca.findByIdAndUpdate({_id:id},{
-                marca: data.marca,
+                titulo: data.titulo,
                 
             })
             res.status(200).send({data:reg});
@@ -97,6 +97,15 @@ const eliminar_marca_admin = async function(req, res){
 
             var id = req.params['id']
 
+            // // Primero, verifica si la marca está asignada a algún producto
+            // const marcaAsignada = await producto.findOne({ marca: id });
+
+            // if (marcaAsignada) {
+            //     // Si la marca está asignada a un producto, no la elimines y devuelve un mensaje de error
+            //     res.status(400).send({ message: 'La marca está asignada a un producto y no puede eliminarse.' });
+            // } else {
+            //     // Si la marca no está asignada a ningún producto, elimínala
+            // }
             let reg=await marca.findByIdAndRemove({_id:id});
             res.status(200).send({data:reg});
             
@@ -118,115 +127,3 @@ module.exports = {
     actualizar_marca_admin,
     eliminar_marca_admin
 }
-
-// var Marca = require('../models/marca');
-
-// function registrar(req,res){
-//     var data = req.body;
-
-//     var marca = new Marca();
-//     marca.titulo = data.titulo;
-//     Marca.find({titulo: new RegExp(marca.titulo,'i')}, (err,marca_listado)=>{
-//         if(err){
-//             res.status(500).send({message: err});
-//         }else{
-//             console.log(marca_listado);
-//             if(marca_listado.length > 0){
-//                 res.status(200).send({message: 'Ya existe esta Marca en la Base de Datos', status : '01'}); 
-//             }else{
-//                 marca.save((err,marca_save)=>{
-//                     if(err){
-//                         console.log(err);
-//                         console.log('Operacion Mala');
-//                         res.status(500).send({message: 'Error en el servidor'});
-//                     }else{
-//                         if(marca_save){
-//                             res.status(200).send({marca: marca_save, status : '00'});
-//                         }else{
-//                             res.status(403).send({message: 'La marca no se pudo registrar'});
-//                         }
-//                     }
-//                 });
-//             }
-//         }
-//     });
-   
-// }
-
-// function obtener_marca(req,res){
-//     var id = req.params['id'];
-    
-//     Marca.findById({_id: id}, (err,marca_data) =>{
-//         if(err){
-//             res.status(500).send({message: 'Error en el servidor'});
-//         }else{
-//             if(marca_data){
-//                 res.status(200).send({marca: marca_data});
-//             }else{
-//                 res.status(403).send({message: 'La marca no existe'});
-//             }
-//         }
-//     });
-    
-// }
-
-// function editar(req,res){
-//     var id = req.params['id'];
-//     var data = req.body;
-
-//     Marca.findByIdAndUpdate({_id:id},{titulo: data.titulo},(err,marca_edit)=>{
-//         if(err){
-//             res.status(500).send({message: 'Error en el servidor'});
-//         }else{
-//            if(marca_edit){
-//             res.status(200).send({marca: marca_edit});
-//            }else{
-//             res.status(403).send({message: 'La marca no se pudo actualizar'});
-//            }
-//         }
-//     });
-// }
-
-// function eliminar(req,res)
-// {
-//     var id = req.params['id'];
-
-//     Marca.findByIdAndRemove({_id:id},(err,marca_delete)=>{
-//         if(err){
-//             res.status(500).send({message: 'Error en el servidor'});
-//         }else{
-//             if(marca_delete){
-//                 res.status(200).send({marca: marca_delete});
-//             }else{
-//                 res.status(403).send({message: 'La marca no se pudo eliminar'}); 
-//             }
-//         }
-//     });
-// }
-
-// function listar(req,res){
-//     var nombre = req.params['nombre'];
-
-    
-
-//     Marca.find({titulo: new RegExp(nombre,'i')}, (err,marca_listado)=>{
-//         if(err){
-//             res.status(500).send({message: err});
-//         }else{
-//             if(marca_listado){
-//                 res.status(200).send({marca: marca_listado});
-//             }else{
-//                 res.status(403).send({message: 'No hay registros con ese titulo'}); 
-//             }
-//         }
-//     });
-
-// }
-
-// module.exports = {
-//     registrar,
-//     obtener_marca,
-//     editar,
-//     eliminar,
-//     listar
-// }
--- routes/categoria.js
+++ routes/categoria.js
@@ -7,21 +7,13 @@ var CategoriaController = require('../controllers/CategoriaController');
 var api = express.Router();
 var auth = require('../middlewares/authenticate');
 
-api.post('/registro_categoria_admin',auth.auth,CategoriaController.registro_categoria_admin);
-api.get('/listar_categoria_admin/:filtro?',auth.auth,CategoriaController.listar_categoria_admin);
+
+api.get('/listar_categoria_filtro_admin/:tipo/:filtro?',auth.auth,CategoriaController.listar_categoria_filtro_admin);
+api.post('/registro_categoria_admin/',auth.auth,CategoriaController.registro_categoria_admin);
+api.get('/obtener_categoria_admin/:id', auth.auth,CategoriaController.obtener_categoria_admin);
+api.put('/actualizar_categoria_admin/:id',auth.auth,CategoriaController.actualizar_categoria_admin);
 api.delete('/eliminar_categoria_admin/:id',auth.auth,CategoriaController.eliminar_categoria_admin);
+
 
 module.exports = api
 
-// var express = require('express');
-// var categoriaController = require('../controllers/CategoriaController');
-
-// var api = express.Router();
-
-// api.post('/categoria/registrar',categoriaController.registrar);
-// api.get('/categoria/:id',categoriaController.obtener_categoria);
-// api.put('/categoria/editar/:id',categoriaController.editar);
-// api.delete('/categoria/eliminar/:id',categoriaController.eliminar);
-// api.get('/categorias/:nombre?',categoriaController.listar);
-
-// module.exports = api;
