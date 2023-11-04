e89a65e972fd97a5b191c11f59e11ac78b32629c
Sair Ayestas <115195289+JosEd23@users.noreply.github.com>
2023-11-04 00:22:29 -0600

    cambios

--- models/detallecompra.js
+++ models/detallecompra.js
@@ -7,7 +7,7 @@ var DetalleCompraSchema = Schema({
     idproducto: {type: Schema.ObjectId, ref: 'producto', require: true},
     cantidad: {type: Number, require:true },
     fecha_exp: {type: String, require:true},
-    encabezado_compra: {type:Schema.ObjectId, ref:'compra'},\
+    encabezado_compra: {type:Schema.ObjectId, ref:'compra'},
     createAt: {type: Date, default: Date.now}
 });
 
